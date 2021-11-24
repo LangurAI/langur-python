5d4257d7945624dfdaa8f30986a4abbda7165330
khknopp <k.h.knopp@student.tue.nl>
2021-11-24 20:07:04 +0100

    Added neuron classes

--- modules/neurons.py
+++ modules/neurons.py
@@ -1,5 +1,26 @@
+import activation, initialization
+from numpy import append
+
 class Neuron:
+    """
+    This class is the parent of all of the neuron types.
+    They all inherit from this class.
+    """
     pass
 
 class Perceptron(Neuron):
-    pass
+    """
+    This class describes the Perceptron - the 
+    basic neuron.
+    """
+    def __init__(self, input_size=5, activation=activation.step, initialization=initialization.He, bias_initialization=initialization.Zeros, bias=bias.One):
+        self.bias = bias
+        self.weights = append(bias_initialization(1), initialization(input_size-1))
+        self.activation = activation
+        self.input_size = input_size
+
+    def __str__(self):
+        return f"{self.weights}"
+
+p = Perceptron()
+print(p)
