0df2590039ffd40ef259268f147431c4c1940f66
khknopp <k.h.knopp@student.tue.nl>
2021-11-24 20:34:59 +0100

    Added input_size to parent neuron class

--- modules/neurons.py
+++ modules/neurons.py
@@ -6,18 +6,19 @@ class Neuron:
     This class is the parent of all of the neuron types.
     They all inherit from this class.
     """
-    pass
+    def __init__(self, input_size):
+        self.input_size = input_size
 
 class Perceptron(Neuron):
     """
     This class describes the Perceptron - the 
     basic neuron.
     """
-    def __init__(self, input_size=5, activation=activation.step, initialization=initialization.He, bias_initialization=initialization.Zeros, bias=bias.One):
+    def __init__(self, input_size=5, activation=activation.Step, initialization=initialization.He, bias_initialization=initialization.Zeros, bias=1):
+        super().__init__(input_size)
         self.bias = bias
         self.weights = append(bias_initialization(1), initialization(input_size-1))
         self.activation = activation
-        self.input_size = input_size
 
     def __str__(self):
         return f"{self.weights}"
