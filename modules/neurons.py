67a378ef9741b5042e5828a9fcafb2f7d7ca8788
Kajetan Knopp <kajetan@knopp.com.pl>
2021-12-26 20:34:32 +0100

    Added uni and bi-sigmoidal neurons

--- modules/neurons.py
+++ modules/neurons.py
@@ -19,6 +19,28 @@ class Perceptron(Neuron):
         self.bias = bias(1)
         self.weights = initialization(input_size)
         self.activation = activation
+        self.learning_rate = learning_rate
+
+    def calculate(self, input_values):
+        """
+        Calculates the activation function output on a given
+        input.
+        """
+        return self.activation(sum(self.weights * (append(self.bias, input_values))))
+
+    def __str__(self):
+        return f"{self.weights}"
+
+class Sigmoid(Perceptron):
+    """
+    This class describes a sigmoid neuron
+    implementation with a continuous activation
+    function (sigmoid or tanh).
+    """
+
+    def __init__(self, input_size=5, activation=activation.Sigmoid, initialization=initialization.He, bias=initialization.Zeros, learning_rate=1):
+        Perceptron.__init__(self, input_size, activation, initialization, bias, learning_rate)
+
 
     def calculate(self, input_values):
         """
